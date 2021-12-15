96372d6ca2ffe365e65eae5772a18067eb9c4d7e
Aqu <sheenix15@gmail.com>
2021-12-15 22:44:29 +0800

    Changed MOD_ID to MODID

--- src/main/java/io/github/spartan_awakens/SpartanAwakens.java
+++ src/main/java/io/github/spartan_awakens/SpartanAwakens.java
@@ -22,12 +22,12 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(SpartanAwakens.MOD_ID)
+@Mod(SpartanAwakens.MODID)
 public class SpartanAwakens {
 
     public static SpartanAwakens instance;
 
-    public static final String MOD_ID = "spartan_awakens";
+    public static final String MODID = "spartan_awakens";
     public static final String MOD_NAME = "Spartan Awakens";
     private static final Logger LOGGER = LogManager.getLogger();
 
